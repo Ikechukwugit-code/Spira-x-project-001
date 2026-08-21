20646a9dc8b0dfd3126cf1c0f7b0bbb2095a95ed
SPIRA-X <ikechukwugit@gmail.com>
2026-08-21 04:14:39 +0100

    Refactor request engine into functions

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-print("SPIRA-X project 001 is running") 
-name = input("What is your name? ")
-problem = input("What business problem do you want to solve? ")
-industry = input("What is industry are you in? ")
-country = input("What country is the business located? ")
-print()
-print("REQUEST RECEIVED")
-print("---------------")
-print(f"customer: {name}")
-print(f"problem: {problem}")
-print(f"Industry: {industry}")
-print(f"Country: {country}")
-print()
-print("SPIRA-X is processing your request...")
+def collect_request():
+    print("=== SPIRA-X BUSINESS REQUEST ENGINE ===")
+
+    name = input("What is your name? ")
+    industry = input("What industry are you in? ")
+    problem = input("What business problem do you want to solve? ")
+    country = input("Where is your business located? ")
+
+    return name, industry, problem, country  
+
+def display_request(name,industry,problem,country):
+    print()
+    print("REQUEST RECEIVED")
+    print("-----------------")
+    print(f"customer: {name}")
+    print(f"Industry: {industry}")
+    print(f"Problem: {problem}")
+    print(f"Country: {country}")
+    print()
+    print("SPIRA-X is processing your request...")
+
+name,industry,problem,country = collect_request()
+
+display_request(name,industry,problem,country)
